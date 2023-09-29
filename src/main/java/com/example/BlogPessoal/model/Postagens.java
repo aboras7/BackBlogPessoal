aad246ad5f52c7f0e0c374efe9891abee25b75dd
Eduardo Nogueira <eduardo7_@hotmail.com>
2023-09-29 14:01:55 -0300

    Model, Controller e Repository criadas

---
+++ src/main/java/com/example/BlogPessoal/model/Postagens.java
@@ -0,0 +1,60 @@
+package com.example.BlogPessoal.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagens {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Atributo título é obrigatório!")
+    @Size(min = 2, max = 255)
+    private String titulo;
+
+    @NotBlank(message = "Atributo texto é obrigatório!")
+    @Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres.")
+    private String texto;
+
+    @UpdateTimestamp
+    private LocalDateTime data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/com/example/BlogPessoal/repository/PostagensRepository.java
@@ -0,0 +1,9 @@
+package com.example.BlogPessoal.repository;
+
+import com.example.BlogPessoal.model.Postagens;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostagensRepository extends JpaRepository<Postagens, Long> {
+
+    
+}
